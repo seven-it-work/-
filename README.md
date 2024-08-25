7eee174e89adc2a5ea37e36292297fd701d5652a
seven <sevenyjl@gmail.com>
2024-08-25 14:59:59 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,17 @@
+# 参考来源
+https://github.com/VictorChow/SlayTheSpireTrainer
+
+# SlayTheSpireTrainer
+杀戮尖塔对局存档修改
+
+![](./screenshot.png)
+
+# 问题补充
+
+1、有些东西没有更新
+[cards.js](src%2Fconfig%2Fcards.js)
+[potions.js](src%2Fconfig%2Fpotions.js)
+[relics.js](src%2Fconfig%2Frelics.js)
+
+2、筛选加强
+可以根据 花费、描述的模糊查询等条件筛选
---
+++ src/components/Upload.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <el-input v-model:value="text"></el-input>
+  <el-button @click="doIt">解析</el-button>
+</div>
+</template>
+
+<script>
+// import {Base64} from 'js-base64'
+
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  },
+  methods: {
+    doIt() {
+      let json = JSON.parse(this.text)
+      this.$store.commit('setGameData', json)
+      this.$router.push('Home')
+    }
+    // getUploadFile(file) {
+    //   let fileReader = new FileReader()
+    //   fileReader.readAsText(file)
+    //   fileReader.onload = e => this.decodeData(fileReader.result)
+    //   return false
+    // },
+    // decodeData(text) {
+    //   let key = ['k', 'e', 'y']
+    //   let result = Base64.decode(text)
+    //     .split('')
+    //     .map(c => c.charCodeAt(0))
+    //   for (let i = 0; i < result.length; i++) {
+    //     result[i] = result[i] ^ key[i % key.length].charCodeAt(0)
+    //   }
+    //   let json = JSON.parse(String.fromCharCode(...result))
+    //
+    //   this.$store.commit('setGameData', json)
+    //   this.$router.push('Home')
+    // }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
